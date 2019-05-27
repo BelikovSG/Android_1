0b0e85f84fd553ffb37091e7c7e647d8e5afad48
Taemina <Olyzub.96@mail.ru>
2019-05-27 22:19:51 +0700

    Сделала ДЗ к уроку №4

--- Lesson1/app/src/main/java/com/example/lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/example/lesson1/MainActivity.java
@@ -1,55 +1,52 @@
 package com.example.lesson1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-   private EditText etEnteredCity;
-    private Button ChoiceCity;
-
-    private String CityName;
     public static final String NAME_CITY = "NAME_CITY";
+    private EditText etEnteredCity;
+    private Button choiceCity;
+    private String cityName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.MyTheme);
         setContentView(R.layout.activity_main);
         initViews();
-        ChoiceCity.setOnClickListener(this);
-
+        choiceCity.setOnClickListener(this);
     }
-    private void initViews() {
-       ChoiceCity = findViewById(R.id.CityButton);
-        etEnteredCity=findViewById(R.id.CityEdit);
 
-
-    }
     @Override
     public void onClick(View v) {
-      switch (v.getId()) {
-            case R.id.CityButton:
-                Intent nextActivity = new Intent(this, SecondActivity.class);
-                nextActivity.putExtra(NAME_CITY, String.valueOf(etEnteredCity.getText()));
-                startActivity(nextActivity);
-                break;
-
-
+        if (v.getId() == R.id.CityButton) {
+            Intent nextActivity = new Intent(this, SecondActivity.class);
+            nextActivity.putExtra(NAME_CITY, String.valueOf(etEnteredCity.getText()));
+            startActivity(nextActivity);
         }
     }
 
-    @Override protected void onSaveInstanceState(Bundle outState) {
-          super.onSaveInstanceState(outState);
-          outState.putString(NAME_CITY, String.valueOf(etEnteredCity.getText()));
-        }
-        @Override protected void onRestoreInstanceState(Bundle savedInstanceState) {
-         super.onRestoreInstanceState(savedInstanceState);
-            CityName = savedInstanceState.getString(NAME_CITY);
-            etEnteredCity.setText(String.valueOf(CityName));
-        }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(NAME_CITY, String.valueOf(etEnteredCity.getText()));
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        cityName = savedInstanceState.getString(NAME_CITY);
+        etEnteredCity.setText(String.valueOf(cityName));
+    }
 
+    private void initViews() {
+        choiceCity = findViewById(R.id.CityButton);
+        etEnteredCity = findViewById(R.id.CityEdit);
+    }
 }
