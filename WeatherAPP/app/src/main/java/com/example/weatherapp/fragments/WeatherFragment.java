61efc57b915ea1a63ca400824f766ebf25fe4209
Taemina <Olyzub.96@mail.ru>
2019-06-04 17:14:49 +0700

    ДЗ к уроку №6

--- WeatherAPP/app/src/main/java/com/example/weatherapp/fragments/WeatherFragment.java
+++ WeatherAPP/app/src/main/java/com/example/weatherapp/fragments/WeatherFragment.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.weatherapp.R;
 import com.example.weatherapp.model.CityIndex;
@@ -20,11 +19,15 @@ import com.example.weatherapp.model.CityIndex;
 public class WeatherFragment extends Fragment {
     public static final String PARCEL = "parcel";
     private static String TAG = "WeatherFragment";
+    public static String CITY = "CITY";
     private ImageView weather;
     private TextView cityNameView;
+    private TextView temperatureView;
     private TypedArray weatherImgs;
 
+
     private int defaultIndex = 0;
+
     public static WeatherFragment createInstance(CityIndex parcel) {
         WeatherFragment fragment = new WeatherFragment();
         Bundle args = new Bundle();
@@ -39,7 +42,8 @@ public class WeatherFragment extends Fragment {
         return inflater.inflate(R.layout.weather_list, container, false);
     }
 
-    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         if (getView() == null) {
             return;
@@ -47,16 +51,19 @@ public class WeatherFragment extends Fragment {
 
         weather = getView().findViewById(R.id.iv_coat_weather);
         cityNameView = getView().findViewById(R.id.tv_chosen_city);
+        temperatureView = getView().findViewById(R.id.tv_city);
         weatherImgs = getResources().obtainTypedArray(R.array.weather_list);
-
         CityIndex parcel = getParcel();
         if (parcel != null) {
             weather.setImageResource(weatherImgs.getResourceId(parcel.getIndex(), defaultIndex));
             cityNameView.setText(parcel.getCityName());
+            temperatureView.setText(getResources().getTextArray(R.array.weather_temp_list)[parcel.getIndex()].toString());
         } else {
             weather.setImageResource(weatherImgs.getResourceId(defaultIndex, defaultIndex));
             cityNameView.setText(getResources().getTextArray(R.array.cities_list)[defaultIndex].toString());
+            temperatureView.setText(getResources().getTextArray(R.array.weather_temp_list)[defaultIndex].toString());
         }
+        CITY = cityNameView.getText().toString();
 
     }
 
